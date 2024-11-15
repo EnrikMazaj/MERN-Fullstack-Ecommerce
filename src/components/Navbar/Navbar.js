0cf5831147213e744a383b8f99532f381feb6ac5
Enrik Mazaj <enrik.mazai@outlook.com>
2024-11-15 02:05:41 +0200

    Home Page Content, Slideshow, Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,28 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css'
+import "./Navbar.css";
+import LoginModal from "../LoginModal/LoginModal";
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/Routes">Routes</Link></li>
-        <li><Link to="/Tickets">Tickets</Link></li>
-        <li><Link to="/Contact">Contact</Link></li>
+    <div className="navbar">
+      <img src={`${process.env.PUBLIC_URL}/assets/ktel.png`} alt="Ktel Logo" />
+      <ul className="list">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Routes">Routes</Link>
+        </li>
+        <li>
+          <Link to="/Tickets">Tickets</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
       </ul>
+      <LoginModal />
     </div>
-  )
+  );
 };
 
 export default Navbar;
---
+++ src/components/Slideshow/Slideshow.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Slideshow.css";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+const Slideshow = () => {
+  const images = [
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/cc/8d/2a/the-beautiful-mylopotamos.jpg?w=700&h=-1&s=1",
+    "https://wowiwalkers.com/wp-content/uploads/2021/10/Thessaloniki-Photoshop.jpg",
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  const nextSlide = () => {
+    setIndex((index) => (index + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setIndex((index) => (index - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div className="slideshow-container">
+      <div className="slide">
+        <img src={images[index]} alt="Slide" />
+      </div>
+      <button className="prev" onClick={prevSlide}>
+        <FaArrowLeft />
+      </button>
+      <button className="next" onClick={nextSlide}>
+        <FaArrowRight />
+      </button>
+      <div className="index-number">
+      <p>{index + 1} / {images.length}</p>
+    </div>
+    </div>
+  );
+};
+
+export default Slideshow;
