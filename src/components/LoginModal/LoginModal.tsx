6d7df4b6af26084c1d358672812845ed9124be80
Enrik Mazaj <enrik.mazai@outlook.com>
2025-05-18 13:56:05 +0300

    fix: add api url

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -107,7 +107,7 @@ const LoginModal = () => {
     setIsRegisterLoading(true);
 
     try {
-      const response = await fetch('https://bus-ecommerce.onrender.com/api/users/register', {
+      const response = await fetch(`${API_URL}/api/users/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
